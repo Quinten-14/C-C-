9c67163d887c8392a0182303f8258ba5707ef620
Quinten-14 <67979828+Quinten-14@users.noreply.github.com>
2024-06-02 19:06:15 +0200

    Added general data for transactions

---
+++ CPP_Bank_System/Class/Transaction.hpp
@@ -0,0 +1,16 @@
+#ifndef TRANSACTION_HPP
+# define TRANSACTION_HPP
+
+class  Transaction
+{
+    public:
+
+    private:
+        int     m_id;
+        int     m_from;
+        int     m_to;
+        double  m_amount;
+        std::string m_message;
+};
+
+#endif
